9d528f15ef7d1eb328cc4014eb29b49e6cee19ce
Travis Weir <tweirtx@gmail.com>
2023-05-12 18:02:32 -0500

    Initial commit of dumb testing things

---
+++ test.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+
+someengine = sqlalchemy.create_engine("postgresql://dozer:simplepass@localhost/dozer")
+backupengine = sqlalchemy.create_engine("mysql+mysqldb://test:asdfasdf@localhost/test")
+
+metadata_obj = sqlalchemy.MetaData()
+metadata_obj.reflect(bind=someengine)
+metadata_obj.create_all(backupengine, checkfirst=True)
+with backupengine.connect() as backup_connection:
+    for table in reversed(metadata_obj.sorted_tables):
+        table_select = sqlalchemy.sql.expression.select(table.columns)
+        table_as_connection = someengine.connect().execute(table_select)
+        # table.create(backup_connection)
+        for row in table_as_connection:
+            print(row)
+            insert = sqlalchemy.sql.expression.insert(table).values(row)
+            backup_connection.execute(insert)
