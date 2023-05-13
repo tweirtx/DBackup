8e90c265038d1775c0f3bcb41c2ef647942dc05d
Travis Weir <tweirtx@gmail.com>
2023-05-12 19:02:49 -0500

    (not working) trying to autoload with backup engine

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ backupengine = sqlalchemy.create_engine("sqlite://")
 
 metadata_obj = sqlalchemy.MetaData()
 metadata_obj.reflect(bind=someengine)
-metadata_obj.create_all(backupengine, checkfirst=True)
+
+other_metadata = sqlalchemy.MetaData()
+
 with backupengine.connect() as backup_connection:
     for table in reversed(metadata_obj.sorted_tables):
+        table_obj = sqlalchemy.Table(table.name, other_metadata, autoload_with=backup_connection)
+        table_obj.create(backup_connection)
         table_select = sqlalchemy.sql.expression.select(table.columns)
         table_as_connection = someengine.connect().execute(table_select)
-        # table.create(backup_connection)
         for row in table_as_connection:
             print(row)
             insert = sqlalchemy.sql.expression.insert(table).values(row)
